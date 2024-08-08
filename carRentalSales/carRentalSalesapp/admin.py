e4e59d7d2c68cfd58dd3a37a918aa4755c9898f3
Your name <mtrunghieu12@gmail.com>
2024-08-08 23:50:16 +0700

    Update models and admin CateCar, Car

--- carRentalSales/carRentalSalesapp/admin.py
+++ carRentalSales/carRentalSalesapp/admin.py
@@ -1,3 +1,45 @@
+from cloudinary.models import CloudinaryResource
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.utils.html import mark_safe
+from .models import User, Category, Car, Image
 
-# Register your models here.
+
+class HCarAppAdminSite(AdminSite):
+    site_title = 'Trang quản trị HCAR'
+    site_header = 'Hệ thống Quản lý cửa hàng XE Ô TÔ HCAR'
+    index_title = 'Trang chủ quản trị'
+
+
+class CateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'Hãng_Xe']
+    readonly_fields = ['Hãng_Xe']
+    search_fields = ['name']
+
+    def Hãng_Xe(self, obj):
+        if obj.image:
+            if type(obj.image) is CloudinaryResource:
+                return mark_safe(
+                    f'<img src="{obj.image.url}" height="200"  width="300" alt="avatar" />'
+                )
+            return mark_safe(
+                f'<img src="{obj.image.name}" height="200" width="300" alt="avatar" />'
+            )
+
+
+class ImageInlineAdmin(admin.StackedInline):
+    model = Image
+    extra = 0
+    fk_name = 'car'
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+    inlines = [ImageInlineAdmin, ]
+
+
+admin_site = HCarAppAdminSite(name='myHCar')
+
+admin.site.register(Category, CateAdmin)
+admin.site.register(Car, CarAdmin)
--- carRentalSales/carRentalSalesapp/models.py
+++ carRentalSales/carRentalSalesapp/models.py
@@ -24,11 +24,22 @@ class Category(BaseModel):
     name = models.CharField(max_length=30)
     image = CloudinaryField(folder="cateCar", null=False, blank=False, default='')
 
+    def __str__(self):
+        return self.name
+
 
 class Car(BaseModel):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    model = models.CharField(max_length=100)
+    name = models.CharField(max_length=30, default='')
+    model = models.CharField(max_length=100)  # kiểu dáng, phiên bản
+    fuel_type = models.CharField(max_length=50, default='')  # loại nhiên lieu
+    transmission = models.CharField(max_length=50, default='')  #loại hộp số
+    seats = models.IntegerField(default=0)
+    interior_color = models.CharField(max_length=50, default='')  # màu noi thất
+    condition = models.CharField(max_length=10, default='')  # tình trạng (new - used)
+    origin = models.CharField(max_length=10, default='')  # xuất  xứ (domestic - imported)
+    color = models.CharField(max_length=50, default='')
     year = models.IntegerField()
     quantity = models.IntegerField()
     description = models.TextField(null=True, blank=True)
@@ -48,6 +59,7 @@ class Rental(BaseModel):
     price_per_day = models.FloatField(null=True, blank=True)
     mileage = models.IntegerField()
     deposit_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_rental_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # dễ thống kê doanh thu
 
 
 class Review(BaseModel):
